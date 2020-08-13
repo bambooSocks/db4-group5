f8032bb4dd88e36eaea232395242a101e39648fb
Viktor Tsanev <viktor.tzanev@gmail.com>
2020-08-13 22:34:31 +0200

    OLED screen - show temp

--- display_example.py
+++ display_example.py
@@ -1,10 +1,11 @@
 import ssd1306
 from machine import I2C, Pin
+from temp_control import T_control
 
+t_control = T_control(14,32,12,34)
 i2c = I2C(scl=Pin(22), sda=Pin(23), freq=100000)
 oled = ssd1306.SSD1306_I2C(128, 32, i2c)
 oled.fill(0)
-oled.text("I", 0, 8)
-oled.text("wrote", 8, 16)
-oled.text("this!", 16, 24)
+oled.text("Temperature", 0, 0) # x, y, column
+oled.text(str(t_control.thermometer.read()), 10, 10)
 oled.show()
